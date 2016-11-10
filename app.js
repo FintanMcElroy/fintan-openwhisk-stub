c1cf4938b129d46600b8042536f136417318a9f0
unknown <fintan@R9022LYC.hursley.ibm.com>
2016-11-10 16:57:41 +0000

    Initial

---
+++ app.js
@@ -0,0 +1,54 @@
+/*eslint-env node*/
+
+//------------------------------------------------------------------------------
+// node.js starter application for Bluemix
+//------------------------------------------------------------------------------
+
+// This application uses express as its web server
+// for more info, see: http://expressjs.com
+var express = require('express');
+
+// cfenv provides access to your Cloud Foundry environment
+// for more info, see: https://www.npmjs.com/package/cfenv
+var cfenv = require('cfenv');
+
+// create a new express server
+var app = express();
+
+// serve the files out of ./public as our main files
+app.use(express.static(__dirname + '/public'));
+
+// get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
+
+// Connect to OpenWhisk
+var openwhisk = require('openwhisk');
+var ow = openwhisk({
+	api: 'https://openwhisk.ng.bluemix.net/api/v1/',
+	//api_key: '622afc08-27ac-4a05-b5b6-2d7c3408df25:GNmeEWxqQhNF0mY9WhvcBOuL9tu9sDm6bnIsTVoXOPrXIwFuVTyOdFkL3OGLxLax';
+	api_key: '622afc08-27ac-4a05-b5b6-2d7c3408df25:GNmeEWxqQhNF0mY9WhvcBOuL9tu9sDm6bnIsTVoXOPrXIwFuVTyOdFkL3OGLxLax',
+	namespace: 'fintan_mcelroy%40uk.ibm.com_fintan_us'});
+
+app.get("/invoke/:action", function(req, res) {
+	ow.actions.invoke({
+		blocking: true,
+		actionName: req.params.action,
+		params: req.query
+	}).then(function(param) {
+		console.log("Returned from ow.actions.invoke with param : " + param);
+		console.log("Returned from ow.actions.invoke with param.response.result : " + param.response.result);
+		// Return the result of the OpenWhisk call
+		res.send(JSON.stringify(param.response.result));
+	}).catch(function(err) {
+		console.log("ERROR Returned from ow.actions.invoke with err : " + err);
+		res.send("Error: " + JSON.stringify(err));
+	});
+
+});
+
+// start server on the specified port and binding host
+app.listen(appEnv.port, '0.0.0.0', function() {
+
+	// print a message when the server starts listening
+  console.log("server starting on " + appEnv.url);
+});
