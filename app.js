25eb53ae873c874f2ebcca0cec943b432b665c99
Fintan McElroy <fintan_mcelroy@uk.ibm.com>
2016-12-10 08:35:41 +0000

    changes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 /*eslint-env node*/
+'use strict'
 
 //------------------------------------------------------------------------------
 // node.js starter application for Bluemix
@@ -22,35 +23,56 @@ app.use(express.static(__dirname + '/public'));
 var appEnv = cfenv.getAppEnv();
 
 // Connect to OpenWhisk
-var openwhisk = require('openwhisk');
-var ow = openwhisk({
+const openwhisk = require('openwhisk');
+const ow = openwhisk({
 	api: 'https://openwhisk.ng.bluemix.net/api/v1/',
-	//api_key 'NjIyYWZjMDgtMjdhYy00YTA1LWI1YjYtMmQ3YzM0MDhkZjI1OkdObWVFV3hxUWhORjBtWTlXaHZjQk91TDl0dTlzRG02Ym5Jc1RWb1hPUHJYSXdGdVZUeU9kRmtMM09HTHhMYXg='
 	//api_key: '622afc08-27ac-4a05-b5b6-2d7c3408df25:GNmeEWxqQhNF0mY9WhvcBOuL9tu9sDm6bnIsTVoXOPrXIwFuVTyOdFkL3OGLxLax';
 	api_key: '622afc08-27ac-4a05-b5b6-2d7c3408df25:GNmeEWxqQhNF0mY9WhvcBOuL9tu9sDm6bnIsTVoXOPrXIwFuVTyOdFkL3OGLxLax',
-	namespace: 'fintan_mcelroy%40uk.ibm.com_fintan_us'});
+	namespace: 'fintan_mcelroy@uk.ibm.com_fintan_us'});
 
 app.get("/invoke/:action", function(req, res) {
+
 	console.log("LOG - Inside the app.get(/invoke/:action) ...");
-	ow.packages.get().then(function(pkg) {
+	/*ow.packages.get({packageName: "fintanCloudant"}).then(function(pkg) {
 		console.log("Returned from ow.packages.get with pkg : " + JSON.stringify(pkg));
-		console.log("Returned from ow.packages.get with pkg.response.result : " + JSON.stringify(pkg.response.result));
+		res.send(JSON.stringify(pkg));
 	}).catch(function(err) {
-		console.log("ERROR Returned from ow.packages.get with err : " + JSON.stringify(err));
+		console.log("ERROR Returned from ow.packages.get with err : " );
+		res.send("Error");
 	});
-	console.log("ow.actions.invoke -> actionName: req.params.action " + req.params.action );
-	console.log("ow.actions.invoke -> params: req.query " + req.query);
+	ow.actions.list().then(function(actions) {
+		console.log("Returned from ow.actions.list : " + JSON.stringify(actions));
+		res.send(JSON.stringify(actions));
+	}).catch(function(err) {
+		console.log("ERROR Returned from ow.actions.list with err : " + JSON.stringify(err));
+		res.send("Error");
+	});
+	ow.actions.get({actionName: "trivial"}).then(function(trivial) {
+		console.log("Returned from ow.actions.get with trivial : " + JSON.stringify(trivial));
+		res.send(JSON.stringify(trivial));
+	}).catch(function(err) {
+		console.log("ERROR Returned from ow.actions.get with err : " + JSON.stringify(err));
+		res.send("Error");
+	});
+	console.log("LOG - About to execute James provided code ...");
+	ow.actions.invoke({actionName: 'trivial', blocking: true}).then(result => {
+  console.dir(result)
+}).catch(err => console.log) */
+
+
+ 	console.log("ow.actions.invoke -> actionName: req.params.action " + req.params.action );
+	console.log("ow.actions.invoke -> params: req.query " + JSON.stringify(req.query));
 	ow.actions.invoke({
 		blocking: true,
 		actionName: req.params.action,
 		params: req.query
 	}).then(function(param) {
-		console.log("Returned from ow.actions.invoke with param : " + param);
-		console.log("Returned from ow.actions.invoke with param.response.result : " + param.response.result);
+		console.log("Returned from ow.actions.invoke with param : " + JSON.stringify(param));
+		console.log("Returned from ow.actions.invoke with param.response.result : " + JSON.stringify(param.response.result));
 		// Return the result of the OpenWhisk call
 		res.send(JSON.stringify(param.response.result));
 	}).catch(function(err) {
-		console.log("ERROR Returned from ow.actions.invoke with err : " + err);
+		console.log("ERROR Returned from ow.actions.invoke with err : " + JSON.stringify(err));
 		res.send("Error: " + JSON.stringify(err));
 	});
 
